61e8268443bcd7194dd80a46decba8d6a021c701
Brian Bornino <brianbornino@gmail.com>
2024-04-28 05:58:33 -0700

    Manually Set the Entry Form Field Lables

--- views/site/entry.php
+++ views/site/entry.php
@@ -4,9 +4,9 @@ use yii\widgets\ActiveForm;
 ?>
 <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name') ?>
+    <?= $form->field($model, 'name')->label('Your Name') ?>
 
-    <?= $form->field($model, 'email') ?>
+    <?= $form->field($model, 'email')->label('Your Email') ?>
 
     <div class="form-group">
         <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
